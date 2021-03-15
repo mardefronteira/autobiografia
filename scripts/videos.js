bd428fb4923264c66bdb627596a014278c2f98b8
mardefronteira <marcelamancino@pm.me>
2021-03-15 18:40:24 -0300

    adição do minidoc

--- scripts/videos.js
+++ scripts/videos.js
@@ -1,30 +1,3 @@
-const videos = [
-  {
-    src: "entrevista_lucri.mp4",
-    poster: "entrevista_lucri.JPG",
-    legenda: "entrevista_lucri.vtt",
-    titulo: "Bate Papo com a Iluminadora Lucri Reggiani de Curitiba PR",
-    desc: `entrevistadora NINA ROSA SÁ<br/>
-    apoio CAROL WINTER<br/>
-    edição NADJA NAIRA<br/>
-    entrevista gravada via zoom em janeiro de 2021<br/>
-    duração 20 minutos
-    `,
-  },
-  {
-    src: "entrevista_semy.mp4",
-    poster: "entrevista_semy.JPG",
-    legenda: "entrevista_semy.vtt",
-    titulo: "Bate Papo com a iluminadora Semy Monastier de Curitiba PR",
-    desc: `entrevistadora NINA ROSA SÁ<br/>
-    apoio CAROL WINTER<br/>
-    edição NADJA NAIRA<br/>
-    entrevista gravada via zoom em janeiro de 2021<br/>
-    duração 35 minutos
-    `,
-  },
-];
-
 function configurarVideos() {
   for (let video of videos) {
     const i = videos.indexOf(video);
