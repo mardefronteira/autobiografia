20b2e480185873eb0c24483a65ea4b60fc2be20f
mardefronteira <mmv341@nyu.edu>
2022-03-13 16:27:26 -0300

    atualização de conteúdo + suporte youtube 03/2022

--- scripts/textos.js
+++ scripts/textos.js
@@ -1,16 +1,34 @@
 function mostrarTexto(texto) {
-  document.querySelector("#src-audio-texto").src = `audios/${texto.audio}`;
-  document.querySelector("#audio-texto").load();
+
+  // imagem
   document.querySelector("#img-texto").src = `img/posters/${texto.img}`;
   document.querySelector("#img-texto").alt = texto.legenda;
   document.querySelector("#legenda-texto").innerHTML = texto.legenda;
+
+  // título
   document.querySelector("#titulo-texto").innerHTML = texto.titulo;
+
+  // autora
   document.querySelector(
     "#autora-texto"
   ).innerHTML = `Escrito por: ${texto.autora}`;
+
+  // player de áudio
+  if (texto.audio !== "") {
+    document.querySelector("#src-audio-texto").src = `audios/${texto.audio}`;
+    document.querySelector("#audio-texto").load();
+    document.querySelector("#div-audio-texto").style = "display:block"
+  } else {
+    document.querySelector("#div-audio-texto").style = "display:none"
+  }
+
+  // resumo
   document.querySelector("#resumo-texto").innerHTML = texto.resumo;
+
+  // artigo (corpo do texto)
   document.querySelector("#corpo-texto").innerHTML = texto.texto;
 
   // mostrar página do texto
   mostrarPagina("texto");
+  
 }
--- scripts/videos.js
+++ scripts/videos.js
@@ -1,11 +1,21 @@
 function mostrarVideo(video) {
-  document.querySelector("#src-video").src = `videos/${video.src}`;
-  video.legenda !== ""
-    ? (document.querySelector("#legenda-video").src = `videos/${video.legenda}`)
-    : (document.querySelector("#legenda-video").src = ``);
-  document.querySelector("#elem-video").poster = `img/posters/${video.poster}`;
+  if (video.src !== "") {
+    document.querySelector("#elem-video").poster = `img/posters/${video.poster}`;
+    document.querySelector("#src-video").src = `videos/${video.src}`;
+    video.legenda !== ""
+      ? (document.querySelector("#legenda-video").src = `videos/${video.legenda}`)
+      : (document.querySelector("#legenda-video").src = ``);
+    document.querySelector("#elem-iframe").style = "display: none;"
+    document.querySelector("#elem-video").style = "display:block";
+  } else {
+    document.querySelector("#elem-iframe").src = `https://www.youtube-nocookie.com/embed/${video.idYoutube}`;
+    document.querySelector("#elem-iframe").style = "display: block;"
+    document.querySelector("#elem-video").style = "display: none";
+  }
+  
   document.querySelector("#titulo-video").innerHTML = video.titulo;
   document.querySelector("#desc-video").innerHTML = video.desc;
+
 
   // mostrar página do vídeo
   esconderTudo();
